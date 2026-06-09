22f39579ee48971786c72107cbe3b453836808c3
Harsh Moreshwar Kale <harshmkale.2004@gmail.com>
2026-06-09 21:10:18 +0530

    Fix static import resolution warning for adk_connectors.telegram by adding static redirection sub-package

---
+++ packages/core/adk_connectors/telegram/__init__.py
@@ -0,0 +1,16 @@
+# Static redirection for VS Code Pylance / type checkers
+from adk_connectors_telegram import (
+    TelegramConfig,
+    TelegramAdapter,
+    TelegramParser,
+    TelegramFormatter,
+    TelegramConnector,
+)
+
+__all__ = [
+    "TelegramConfig",
+    "TelegramAdapter",
+    "TelegramParser",
+    "TelegramFormatter",
+    "TelegramConnector",
+]
